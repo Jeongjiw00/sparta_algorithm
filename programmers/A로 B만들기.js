704a95502adfe02932af49df9d4b90005b07f73a
Jeongjiw00 <wldn0229@naver.com>
2024-01-02 23:29:01 +0900

    A로 B 만들기 수정

--- programmers/A로 B만들기.js
+++ programmers/A로 B만들기.js
@@ -3,21 +3,9 @@
 function solution(before, after) {
   // split - 문자열을 배열로 분할
   // join - 배열을 문자열로 결합
-  if (before.split("").reverse().join("") === after) return 1;
-  else return 0;
-}
-
-// 내림차순 for문으로 반전
-function solution2(before, after) {
-  let newString = "";
-
-  for (let i = before.length - 1; i >= 0; i--) {
-    newString += before[i];
-  }
-
-  if (newString === after) return 1;
+  if (before.split("").sort().join() === after.split("").sort().join())
+    return 1;
   else return 0;
 }
 
 console.log(solution("olleh", "hello"));
-console.log(solution2("olleh", "hello"));
