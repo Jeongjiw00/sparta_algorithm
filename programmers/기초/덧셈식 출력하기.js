8aa71282aa02c1d15a21a2cd8a11090c74a8ecc4
Jeongjiw00 <wldn0229@naver.com>
2024-01-09 01:49:32 +0900

    덧셈식 출력하기

---
+++ programmers/기초/덧셈식 출력하기.js
@@ -0,0 +1,19 @@
+// 4 5  - > 4 + 5 = 9
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  console.log(
+    `${Number(input[0])} + ${Number(input[1])} = ${
+      Number(input[0]) + Number(input[1])
+    }`
+  );
+});
