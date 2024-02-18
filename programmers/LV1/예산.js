b9e50ec26fe8ec892ecbfd5a3e5ad565e1d2c94a
Jeongjiw00 <wldn0229@naver.com>
2024-02-18 17:51:55 +0900

    20240218

---
+++ programmers/LV1/예산.js
@@ -0,0 +1,14 @@
+// 예산
+function solution(d, budget) {
+  let count = 0;
+
+  d.sort((a, b) => a - b);
+
+  for (let i = 0; i < d.length; i++) {
+    if (d[i] > budget) break;
+
+    budget -= d[i];
+    count += 1;
+  }
+  return count;
+}
