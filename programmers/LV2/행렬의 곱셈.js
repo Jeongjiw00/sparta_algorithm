ad4e24b6d50e09d3ab676f65f4b5e37bf3da3b37
Jeongjiw00 <wldn0229@naver.com>
2024-02-26 15:54:39 +0900

    20240226

---
+++ programmers/LV2/행렬의 곱셈.js
@@ -0,0 +1,22 @@
+// 행렬의 곱셈
+function solution(arr1, arr2) {
+  let answer = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    let array = [];
+
+    for (let j = 0; j < arr2[0].length; j++) {
+      let sum = 0;
+
+      for (let k = 0; k < arr1[0].length; k++) {
+        sum += arr1[i][k] * arr2[k][j];
+      }
+
+      array.push(sum);
+    }
+
+    answer.push(array);
+  }
+
+  return answer;
+}
