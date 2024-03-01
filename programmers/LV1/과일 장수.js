8a1ef4ceaa8e93e96afa8bf2d3eabdb8bdc6c90e
Jeongjiw00 <wldn0229@naver.com>
2024-03-01 18:55:22 +0900

    20240301

---
+++ programmers/LV1/과일 장수.js
@@ -0,0 +1,15 @@
+// 과일 장수
+function solution(k, m, score) {
+  let answer = 0;
+  score.sort((a, b) => b - a);
+
+  for (let i = 0; i < score.length; i += m) {
+    let box = score.slice(i, i + m);
+
+    if (box.length == m) {
+      answer += m * box[box.length - 1];
+    }
+  }
+
+  return answer;
+}
