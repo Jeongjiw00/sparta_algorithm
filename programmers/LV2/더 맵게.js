bce9589f533d2b06df5fa102fa113b60c6be1200
Jeongjiw00 <wldn0229@naver.com>
2024-05-16 22:55:22 +0900

    20240516

---
+++ programmers/LV2/더 맵게.js
@@ -0,0 +1,96 @@
+// 더 맵게
+// 효율성 테스트 탈락
+function solution(scoville, K) {
+  const sortedScoville = [...scoville].sort((a, b) => b - a);
+  let mixedCount = 0;
+
+  while (sortedScoville.length >= 2 && sortedScoville.at(-1) < K) {
+    const [first, second] = [sortedScoville.pop(), sortedScoville.pop()];
+    const mixedScov = first + second * 2;
+    sortedScoville.push(mixedScov);
+    sortedScoville.sort((a, b) => b - a);
+    mixedCount++;
+  }
+
+  return sortedScoville.at(-1) >= K ? mixedCount : -1;
+}
+
+// 힙구조
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  // 값을 넣되, 오름차 순 정렬함
+  push(value) {
+    this.heap.push(value);
+    let currentIndex = this.heap.length - 1;
+
+    while (
+      currentIndex > 0 &&
+      this.heap[currentIndex] < this.heap[Math.floor((currentIndex - 1) / 2)]
+    ) {
+      const temp = this.heap[currentIndex];
+      this.heap[currentIndex] = this.heap[Math.floor((currentIndex - 1) / 2)];
+      this.heap[Math.floor((currentIndex - 1) / 2)] = temp;
+      currentIndex = Math.floor((currentIndex - 1) / 2);
+    }
+  }
+
+  // 값을 빼되, 오름차 순 정렬 함
+  pop() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const minValue = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    let currentIndex = 0;
+
+    while (currentIndex * 2 + 1 < this.heap.length) {
+      let minChildIndex =
+        currentIndex * 2 + 2 < this.heap.length &&
+        this.heap[currentIndex * 2 + 2] < this.heap[currentIndex * 2 + 1]
+          ? currentIndex * 2 + 2
+          : currentIndex * 2 + 1;
+
+      if (this.heap[currentIndex] < this.heap[minChildIndex]) {
+        break;
+      }
+
+      const temp = this.heap[currentIndex];
+      this.heap[currentIndex] = this.heap[minChildIndex];
+      this.heap[minChildIndex] = temp;
+      currentIndex = minChildIndex;
+    }
+
+    return minValue;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+}
+
+function solution(scoville, K) {
+  const minHeap = new MinHeap();
+
+  for (const sco of scoville) {
+    minHeap.push(sco);
+  }
+
+  let mixedCount = 0;
+
+  while (minHeap.size() >= 2 && minHeap.peek() < K) {
+    const first = minHeap.pop();
+    const second = minHeap.pop();
+    const mixedScov = first + second * 2;
+    minHeap.push(mixedScov);
+    mixedCount++;
+  }
+
+  return minHeap.peek() >= K ? mixedCount : -1;
+}
