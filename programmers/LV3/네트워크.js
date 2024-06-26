b35cd7748310ed565d6703f5a42b3063b62d441d
Jeongjiw00 <wldn0229@naver.com>
2024-06-26 22:29:39 +0900

    20240626

--- programmers/LV3/네트워크.js
+++ programmers/LV3/네트워크.js
@@ -27,3 +27,26 @@ const dfs = (node, visited, computers) => {
       dfs(i, visited, computers);
   }
 };
+
+// --------------------------------------
+function solution2(n, computers) {
+  let visited = [false];
+  let answer = 0;
+
+  function dfs(i) {
+    visited[i] = true;
+    for (let j = 0; j < computers[i].length; j++) {
+      if (computers[i][j] === 1 && !visited[j]) {
+        dfs(j);
+      }
+    }
+  }
+
+  for (let i = 0; i < computers.length; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      answer++;
+    }
+  }
+  return answer;
+}
