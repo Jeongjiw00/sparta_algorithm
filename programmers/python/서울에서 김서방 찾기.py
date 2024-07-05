8e18610634e755a5badd532af77dbb0366129dfb
Jeongjiw00 <wldn0229@naver.com>
2024-07-05 20:37:13 +0900

    20240705

---
+++ programmers/python/서울에서 김서방 찾기.py
@@ -0,0 +1,9 @@
+# 서울에서 김서방 찾기
+def solution(seoul):
+    return ('김서방은 %d에 있다' %seoul.index('Kim'))
+
+
+# 1
+def findKim(seoul):
+    # 함수를 완성하세요
+    return "김서방은 {}에 있다".format(seoul.index('Kim'))
