43522841b275977eee41c92e3ca4606327fb86ab
Jeongjiw00 <wldn0229@naver.com>
2024-07-19 23:15:09 +0900

    20240719

---
+++ programmers/python/뒤에서 5등 위로.py
@@ -0,0 +1,5 @@
+# 뒤에서 5등 위로
+def solution(num_list):
+    return sorted(num_list)[5:]
+
+print(solution([12, 4, 15, 46, 38, 1, 14, 56, 32, 10]))
---
+++ programmers/python/뒤에서 5등까지.py
@@ -0,0 +1,3 @@
+# 뒤에서 5등까지
+def solution(num_list):
+    return sorted(num_list)[:5]
