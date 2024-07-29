5cb50285facadb13ac5b4bd4b63b52fa191c4d34
Jeongjiw00 <wldn0229@naver.com>
2024-07-29 22:09:59 +0900

    20240729

---
+++ programmers/python/n개 간격의 원소들.py
@@ -0,0 +1,11 @@
+# n개 간격의 원소들
+def solution(num_list, n):
+    answer = []
+    for i in range(0,len(num_list),n):
+        answer.append(num_list[i])
+    return answer
+
+
+
+def solution(num_list, n):
+    return num_list[::n]
