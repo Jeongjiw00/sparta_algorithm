546e999581ffdd1a3e8325300ed4e8e4df373759
Jeongjiw00 <wldn0229@naver.com>
2024-08-22 21:17:08 +0900

    20240822

---
+++ programmers/python/가위 바위 보.py
@@ -0,0 +1,11 @@
+# 가위 바위 보
+def solution(rsp):
+    answer = ''
+    for i in rsp :
+        if i == '0' :
+            answer += '5'
+        elif i == '2' :
+            answer += '0'
+        else :
+            answer += '2'
+    return answer
