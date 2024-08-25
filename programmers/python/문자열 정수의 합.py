47a06e246f3329be3c5f852baa71af737344d6a8
Jeongjiw00 <wldn0229@naver.com>
2024-08-25 20:50:07 +0900

    20240825

---
+++ programmers/python/문자열 정수의 합.py
@@ -0,0 +1,11 @@
+# 문자열 정수의 합
+def solution(num_str):
+    answer = 0
+    for i in num_str:
+        answer+=int(i)
+    return answer
+
+
+
+def solution(num_str):
+    return sum(map(int, num_str))
