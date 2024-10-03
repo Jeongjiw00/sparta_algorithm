6206cf63428c90857bc7477c3c86d680dff759f6
Jeongjiw00 <wldn0229@naver.com>
2024-10-03 19:28:13 +0900

    20241003

---
+++ programmers/python/ad 제거하기.py
@@ -0,0 +1,12 @@
+# ad 제거하기
+def solution(strArr):
+    answer = []
+    for i in strArr:
+        if "ad" not in i:
+            answer.append(i)
+
+    return answer
+
+
+def solution(strArr):
+    return [word for word in strArr if 'ad' not in word]
