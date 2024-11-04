c4336b6c3eecfd98e48a2b7b9ba1656994d185b5
Jeongjiw00 <wldn0229@naver.com>
2024-11-04 21:56:25 +0900

    20241104

---
+++ programmers/python/등차수열의 특정한 항만 더하기.py
@@ -0,0 +1,7 @@
+# 등차수열의 특정한 항만 더하기
+def solution(a, d, included):
+    answer = 0
+    for i in range(len(included)):
+        if included[i] == True:
+            answer += a + i * d
+    return answer
