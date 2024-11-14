2924bb63d116884e88f0381649940b915634d234
Jeongjiw00 <wldn0229@naver.com>
2024-11-14 22:53:13 +0900

    20241114

---
+++ programmers/python/문자열이 몇 번 등장하는지 세기.py
@@ -0,0 +1,7 @@
+# 문자열이 몇 번 등장하는지 세기
+def solution(myString, pat):
+    answer = 0
+    for i, x in enumerate(myString) :
+        if myString[i:].startswith(pat) :
+            answer += 1
+    return answer
