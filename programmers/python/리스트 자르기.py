abb2efc5c3db00bc9b079b8e2b30d7102527dacc
Jeongjiw00 <wldn0229@naver.com>
2024-12-02 20:51:00 +0900

    20241202

---
+++ programmers/python/리스트 자르기.py
@@ -0,0 +1,12 @@
+# 리스트 자르기
+def solution(n, slicer, num_list):
+    a, b, c = slicer
+    b += 1
+    if n == 1:
+        return num_list[0:b]
+    if n == 2:
+        return num_list[a:]
+    if n == 3:
+        return num_list[a:b]
+    if n == 4:
+        return num_list[a:b:c]
