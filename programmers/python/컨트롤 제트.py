4ee145881521b52fb66bd5ef30a9b7eb1b7dc049
Jeongjiw00 <wldn0229@naver.com>
2024-12-06 18:35:05 +0900

    20241206

---
+++ programmers/python/컨트롤 제트.py
@@ -0,0 +1,10 @@
+# 컨트롤 제트
+def solution(s):
+    stack = []
+    for a in s.split():
+        if a != 'Z':
+            stack.append(int(a))
+        else:
+            stack.pop()
+
+    return sum(stack)
