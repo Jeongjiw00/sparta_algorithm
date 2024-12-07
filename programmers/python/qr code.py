19616f14595cc771514e9ccd8ce47e5f3371dbcf
Jeongjiw00 <wldn0229@naver.com>
2024-12-07 19:46:09 +0900

    20241207

---
+++ programmers/python/qr code.py
@@ -0,0 +1,10 @@
+# qr code
+def solution(q, r, code):
+    answer = ''
+    for i in range(len(code)):
+        if i % q == r:
+            answer += code[i]
+    return answer
+
+def solution(q, r, code):
+    return code[r::q]
